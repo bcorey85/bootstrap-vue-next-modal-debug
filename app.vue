37e0bb51cdeea45dba9cb6c89e1decdb218c9563
Brandon Corey <brandon@legalfit.com>
2024-02-09 21:18:03 -0600

    init

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <TestComponent />
+  </div>
+</template>
+
+<script>
+import TestComponent from './components/TestComponent.vue';
+
+export default {
+  components: {
+    TestComponent
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
